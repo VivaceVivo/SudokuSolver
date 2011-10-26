5495466d9116a09007c557a53db33b8f38f51548
VivaceVivo <e@mail.de>
2011-10-26 08:11:11 +0200

    new gui

--- src/main/scala/de/ptr/sudoku/GUI.scala
+++ src/main/scala/de/ptr/sudoku/GUI.scala
@@ -7,26 +7,39 @@ import processing.core.PApplet
 
 class GUI extends PApplet {
 
+  val Grid = 36
+
   val sudoku = new Sudoku() {
-    
     override def dumpSolved() {
       val x = cursor.x
       val y = cursor.y
       var c = 0;
       var r = 0;
 
-      text("x:" + x + " y:" + y + " candidates: " + fieldInColRow(x, y).candidates.mkString(","), 10, 30)
+      paintGrid
+
+//      text("x:" + x + " y:" + y + " candidates: " + fieldInColRow(x, y).candidates.mkString(","), 10, 30)
       rows.foreach {
         row =>
           r = r + 1
           row.fields.foreach {
             f =>
+              stroke(0)
+              fill(0)
               c += 1
               val num: Option[Int] = f.number
-              if (num != None) {
-                text(String.valueOf(num.get), c * 29, r * 29)
-              }else{
-                text(".", c * 29, r * 29)
+              if (c - 1 == x && r - 1 == y) {
+              	fill(255)
+              	stroke(255, 0, 0)
+              	rect(c * Grid, r * Grid, Grid - 2, Grid - 2)
+              }
+              if (num == None) {
+              	fill(255)
+              	rect(c * Grid, r * Grid, Grid - 2, Grid - 2)
+              } else {
+              	fill(0)
+              	stroke(255)
+              	text(String.valueOf(num.get), (c) * Grid + 9, (r + 1) * Grid - 4)
               }
           }
           c = 0;
@@ -34,6 +47,17 @@ class GUI extends PApplet {
       }
     }
 
+  }
+  def paintGrid {
+    stroke(0)
+    for (x <- 1 to 3) {
+      for (y <- 1 to 3) {
+        fill(0)
+        rect(x * Grid * 3 - 2 * Grid - 2, y * Grid * 3 - 2 * Grid - 2, Grid * 3 + 4, Grid * 3 + 4)
+        fill(255)
+        rect(x * Grid * 3 - 2 * Grid + 2, y * Grid * 3 - 2 * Grid + 2, Grid * 3 - 4, Grid * 3 - 4)
+      }
+    }
   }
 
   override def setup {
@@ -72,7 +96,7 @@ class GUI extends PApplet {
     //    val f = loadFont("Ziggurat-48.vlw");
     background(255);
     val f = loadFont("AndaleMono-36.vlw");
-    textFont(f, 36);
+    textFont(f, Grid);
     fill(0);
     //    text("1234567890", 10, 100);
     sudoku.dumpSolved()
